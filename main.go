90e8df11ad682439b40f216b8c7de39dd9b4eabc
Niendhitta Tamia Lassela <lasella@samudrainovasi.co.id>
2022-04-08 09:48:58 +0700

    add fitur 2

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Halo aku dari main")
 	fmt.Println("Halo aku dari development")
 	fmt.Println("Halo aku dari development, nambah fitur 1")
+	fmt.Println("Halo aku dari development, nambah fitur 2")
 }
